d5e3de5e1f0afedd9a222023984d8f278710f780
itc-s23018 <s23018@std.it-college.ac.jp>
2025-10-29 13:59:18 +0900

    すべてのEntityを定義

---
+++ app/src/main/java/com/example/talkeasy/data/entity/MessagesEntity.kt
@@ -0,0 +1,27 @@
+package com.example.talkeasy.data.entity
+
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.Index
+import androidx.room.PrimaryKey
+import java.time.LocalDateTime
+
+@Entity(
+    tableName = "message",
+    foreignKeys = [
+        ForeignKey(
+            entity = TalkEntity::class,
+            parentColumns = ["id"],
+            childColumns = ["talkId"],
+            onDelete = ForeignKey.CASCADE
+        )
+    ],
+    indices = [Index(value = ["talkId"])]
+)
+data class MessageEntity(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    val talkId: Int,
+    val text: String,
+    val createdAt: LocalDateTime,
+)
+
